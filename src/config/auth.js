b05f3318f720e7220838cc94b964e81f1d656df6
adrean paul sorono <adreanpaulsorono@gmail.com>
2026-03-11 15:33:13 +0800

    Rename file

--- src/config/auth.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"; //used to create the Redux global store
-import authReducer from "./authSlice";
-
-export const store = configureStore({
-  reducer: {
-    // state.auth will contain token and user data
-    auth: authReducer,
-  },
-});
