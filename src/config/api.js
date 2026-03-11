2b20c19aae510cc605329178644ae36cb530304b
adrean paul sorono <adreanpaulsorono@gmail.com>
2026-03-11 15:47:23 +0800

    Add response if expired tokens

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { store } from "../config/store";
+import { logout } from "../config/authSlice";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API,
@@ -13,5 +15,21 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+
+api.interceptors.response.use(
+  //if res is success
+  (response) => response,
+  //if back is err
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.log("Token expired");
+      //delete user data
+      store.dispatch(logout());
+      //redirect to signin
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default api;
